6b80d5bd7a080f2f41147f1f54db1cf359c38ec9
Ali Shalabi <ashalabi13@gmail.com>
2021-11-03 20:38:58 -0700

    Started working on js

--- carousel.js
+++ carousel.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+!(function(d)){
+
+  var itemClassName = "carousel_photo";
+      items = d.getElementsByClassName(itemClassName),
+      totalItems = items.length,
+      slide = 0,
+      moving = true;
+
+  function setInitialClasses() {
+    items[totalItems - 1].classList.add("prev");
+    items[0].classList.add("active");
+    items[1].classList.add("next");
+  }
+
+  function setEventListeners() {
+    var next = d.getElementsByClassName("carousel_button--next")[0];
+        prev = d.getElementsByClassName("carousel_button--prev")[0];
+
+
+    next.addEventListener("click", moveNext);
+    prev.addEventListener("click", movePrev);
+
+  }
+
+}(document);
