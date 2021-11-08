c679597b37ee6f648db7277f0c41e04eef9e044b
Ali Shalabi <ashalabi13@gmail.com>
2021-11-08 10:09:36 -0800

    Debuggin

--- carousel.js
+++ carousel.js
@@ -1,4 +1,4 @@
-!(function(d)){
+!(function(d){
 
   var itemClassName = "carousel_photo";
       items = d.getElementsByClassName(itemClassName),
@@ -10,6 +10,8 @@
     items[totalItems - 1].classList.add("prev");
     items[0].classList.add("active");
     items[1].classList.add("next");
+
+    console.log("set initial classes working")
   }
 
   function setEventListeners() {
@@ -53,4 +55,49 @@
     }, 500);
   }
 
-}(document);
+  function moveCarouselTo(slide) {
+    if (!moving) {
+      disableInteraction();
+
+      var newPrevious = slide - 1,
+          newNext = slide + 1,
+          oldPrevious = slide - 2,
+          oldNext = slide + 2;
+
+      if ((totalItems - 1) > 3) {
+        if (newPrevious <= 0) {
+          oldPrevious = (totalItems - 1);
+        } else if (newNext >= (totalItems - 1)) {
+          oldNext = 0;
+        }
+
+        if (slide === 0) {
+          newPrevious = (totalItems - 1);
+          oldPrevious = (totalItems - 2);
+          oldNext = (slide + 1);
+        } else if (slide === (totalItems - 1)) {
+          newPrevious - (slide - 1);
+          newNext = 0;
+          oldNext = 1;
+        }
+
+        items[oldPrevious].className = itemClassName;
+        items[oldNext].className = itemClassName;
+
+        items[newPrevious].className = itemClassName + " prev";
+        items[slide].className = itemClassName + " active";
+        items[newNext].className = itemClassName + " next"
+      }
+    }
+  }
+
+  function initCarousel() {
+    setInitialClasses();
+    setEventListeners();
+
+    moving = false;
+  }
+
+  initCarousel();
+
+}(document));
